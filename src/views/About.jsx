7df4b120aee519f8f8eb58a6c3e073b832fb9964
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:11:03 -0400

    initialize about page

---
+++ src/views/About.jsx
@@ -0,0 +1,24 @@
+// ==================================================
+// File: About.jsx
+// Authors: @gkaracsonyi, @justingetty
+// Purpose: About page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from "react-helmet";
+
+const About = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>About Us</title>
+            </Helmet>
+        
+        </>
+    );
+};
+
+export default About;
