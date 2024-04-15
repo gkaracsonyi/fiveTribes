ddbd3d5c969c63edb7ee723d59408cb113e5d88f
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:41:08 -0400

    initialize Education page

---
+++ src/views/Education.jsx
@@ -0,0 +1,25 @@
+// ==================================================
+// File: Education.jsx
+// Authors: @gkaracsonyi, @JustinGetty
+// Purpose: Educational events page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from "react-helmet";
+
+import './education.css';
+
+const Education = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>Educational Events</title>
+            </Helmet>
+        </>
+    );
+};
+
+export default Education;
