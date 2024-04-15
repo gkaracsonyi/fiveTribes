6e61c119612116dd427c21f85ede1816d67555c5
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:42:57 -0400

    initialize Nonprofit page

---
+++ src/views/Nonprofit.jsx
@@ -0,0 +1,26 @@
+// ==================================================
+// File: Nonprofit.jsx
+// Authors: @gkaracsonyi, @JustinGetty
+// Purpose: Nonprofit events page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from "react-helmet";
+
+import './nonprofit.css';
+
+const Nonprofit = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>Nonprofit Events</title>
+            </Helmet>
+        
+        </>
+    );
+};
+
+export default Nonprofit;
