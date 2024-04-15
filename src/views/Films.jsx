776c927480b2dfae5018d0aafcba6bee2f4ac502
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:41:32 -0400

    initialize Films page

---
+++ src/views/Films.jsx
@@ -0,0 +1,25 @@
+// ==================================================
+// File: Films.jsx
+// Authors: @gkaracsonyi, @JustinGetty
+// Purpose: Independent films page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from 'react-helmet';
+
+import './films.css';
+
+const Films = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>Independent Films</title>
+            </Helmet>
+        </>
+    );
+};
+
+export default Films;
