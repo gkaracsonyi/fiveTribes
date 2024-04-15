51eec09d057c15428afa13b5be691779523c81b3
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 19:01:38 -0400

    initialize Footer element

---
+++ src/elements/Footer.jsx
@@ -0,0 +1,21 @@
+// ==================================================
+// File: Footer.jsx
+// Authors: @gkaracsonyi, @justingetty
+// Purpose: Footer element for FTCP website
+// Updated: April 2024
+// ==================================================
+
+const Footer = () => {
+
+
+
+    return (
+        <>
+            <footer>
+                
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
