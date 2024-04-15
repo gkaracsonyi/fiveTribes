0d33c9cc67ef6a76e2c85296e4898892a88fd403
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 19:01:52 -0400

    initialize Header element

---
+++ src/elements/Header.jsx
@@ -0,0 +1,21 @@
+// ==================================================
+// File: Header.jsx
+// Authors: @gkaracsonyi, @justingetty
+// Purpose: Header element for FTCP website
+// Updated: April 2024
+// ==================================================
+
+const Header = () => {
+
+
+
+    return (
+        <>
+            <header>
+                
+            </header>
+        </>
+    );
+};
+
+export default Header;
