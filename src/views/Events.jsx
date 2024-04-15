8b2e9407209f33b10a5acc0b0a064da10e4c2885
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:41:19 -0400

    initialize Events page

---
+++ src/views/Events.jsx
@@ -0,0 +1,25 @@
+// ==================================================
+// File: Events.jsx
+// Authors: @gkaracsonyi, @JustinGetty
+// Purpose: Events page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from 'react-helmet';
+
+import './events.css';
+
+const Events = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>Events</title>
+            </Helmet>
+        </>
+    );
+};
+
+export default Events;
