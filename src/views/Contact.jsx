07c88a92af967e17e6959c4429acd62292559dc6
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:40:51 -0400

    initialize Contact page

---
+++ src/views/Contact.jsx
@@ -0,0 +1,25 @@
+// ==================================================
+// File: Contact.jsx
+// Authors: @gkaracsonyi, @JustinGetty
+// Purpose: Contact page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from "react-helmet";
+
+import './contact.css';
+
+const Contact = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>Contact</title>
+            </Helmet>
+        </>
+    );
+};
+
+export default Contact;
