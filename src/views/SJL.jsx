c7361c5299ee592256001a5cf779bbe86d9cdba5
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:43:08 -0400

    initialize SJL Racing page

---
+++ src/views/SJL.jsx
@@ -0,0 +1,26 @@
+// ==================================================
+// File: SJL.jsx
+// Authors: @gkaracsonyi, @JustinGetty
+// Purpose: SJL Racing page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from "react-helmet";
+
+import './sjl.css';
+
+const SJL = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>SJL Racing</title>
+            </Helmet>
+        
+        </>
+    );
+};
+
+export default SJL;
