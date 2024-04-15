dae85ac73d1384fb7e67ee8bcf63b97f9095e8f7
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 17:38:48 -0400

    add routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import App from './App.jsx'
+import Home from './Home.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -9,7 +9,16 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<App />} />
+                <Route path='/' element={<Home />} />
+                <Route path='commercials' element={<Commercials />} />
+                <Route path='livestreaming' element={<Livestreaming />} />
+                <Route path='events' element={<Events />} />
+                <Route path='events/education' element={<Education />} />
+                <Route path='events/athletics' element={<Athletics />} />
+                <Route path='events/nonprofit' element={<Nonprofit />} />
+                <Route path='films' element={<Films />} />
+                <Route path='about' element={<About />} />
+                <Route path='sjl' element={<SJL />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
