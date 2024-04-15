848939a204972946be009e7a735c38c9cfd55776
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:40:40 -0400

    initialize Commercials page

---
+++ src/views/Commercials.jsx
@@ -0,0 +1,25 @@
+// ==================================================
+// File: Commercials.jsx
+// Authors: @gkaracsonyi, @JustinGetty
+// Purpose: Page for showcasing commercials on FTCP
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from "react-helmet";
+
+import './commercials.css';
+
+const Commercials = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>Commercials</title>
+            </Helmet>
+        </>
+    );
+};
+
+export default Commercials;
