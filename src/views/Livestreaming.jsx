f3d58b78f70f1dc43df25c4cf4ea899879c42350
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:42:45 -0400

    initialize Livestreaming page

---
+++ src/views/Livestreaming.jsx
@@ -0,0 +1,26 @@
+// ==================================================
+// File: Livestreaming.jsx
+// Authors: @gkaracsonyi, @JustinGetty
+// Purpose: Livestreaming page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from "react-helmet";
+
+import './livestreaming.css';
+
+const Livestreaming = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>Livestreaming</title>
+            </Helmet>
+        
+        </>
+    );
+};
+
+export default Livestreaming;
