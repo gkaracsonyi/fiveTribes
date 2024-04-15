c06cd5d73024218042c5246a974b537b6d89d31e
gkaracsonyi <gkaracso@gmail.com>
2024-04-15 18:40:30 -0400

    initialize Athletics page

---
+++ src/views/Athletics.jsx
@@ -0,0 +1,26 @@
+// ==================================================
+// File: Athletics.jsx
+// Authors: @gkaracsonyi, @justingetty
+// Purpose: Athletic events page for FTCP website
+// Updated: April 2024
+// ==================================================
+
+import { Helmet } from "react-helmet";
+
+import './athletics.css';
+
+const Athletics = () => {
+
+
+
+    return (
+        <>
+            <Helmet>
+                <title>Athletic Events</title>
+            </Helmet>
+        
+        </>
+    );
+};
+
+export default Athletics;
