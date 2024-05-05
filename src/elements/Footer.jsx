eeb5f13377b7f67d038597cb5e3a4494b688fe2d
gkaracsonyi <gkaracso@gmail.com>
2024-05-05 13:04:18 -0400

    start work on footer
    - add logo
    - include helpful page links
    - getting ready to add socials
    
    creating the footer has raised some questions about the color pallet, so we have to sort that out.
    
    need to reach out to colin to find out what else he wants in the footer.

--- src/elements/Footer.jsx
+++ src/elements/Footer.jsx
@@ -5,16 +5,45 @@
 // Updated: April 2024
 // ==================================================
 
+import { useNavigate } from 'react-router-dom';
+
 import './footer.css';
+
+import ftLogo from '/ft-logo.png';
 
 const Footer = () => {
 
-    
+    const navigate = useNavigate();
+
+    const goTo = (dest) => {
+        navigate(`/${dest}`);
+    };
 
     return (
         <>
             <footer>
-                
+                <div className='footerLogoContainer'>
+                    <img src={ftLogo} alt="ftcp logo" />
+                </div>
+                <div className='footerLinks'>
+                    <div className='footerPageLinks'>
+                        <h3>Page Links</h3>
+                        <ul>
+                            <li><button onClick={() => goTo('about')}>About Us</button></li>
+                            <li><button onClick={() => goTo('films')}>Films</button></li>
+                            <li><button onClick={() => goTo('events')}>Events</button></li>
+                            <li><button onClick={() => goTo('commercials')}>Commercials</button></li>
+                            <li><button onClick={() => goTo('livestreaming')}>Livestreaming</button></li>
+                            <li><button onClick={() => goTo('sjl')}>SJL Racing</button></li>
+                        </ul>
+                    </div>
+                    <div className='footerSocialLinks'>
+                        <button><img src={ftLogo} alt="test logo" /></button>
+                        <button><img src={ftLogo} alt="test logo" /></button>
+                        <button><img src={ftLogo} alt="test logo" /></button>
+                        <button><img src={ftLogo} alt="test logo" /></button>
+                    </div>
+                </div>
             </footer>
         </>
     );
