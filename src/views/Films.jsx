cf1b90303f056457b99a9241378297d2b899fb94
gkaracsonyi <gkaracso@gmail.com>
2024-05-13 16:08:34 -0400

    implement cdn for movie banners to decrease media load time

--- src/views/Films.jsx
+++ src/views/Films.jsx
@@ -13,17 +13,17 @@ import './films.css';
 
 const film1_name = 'MIDSOMMAR';
 const film1_description = "A couple travel to Sweden to visit their friend's rural hometown for its fabled midsummer festival, but what begins as an idyllic retreat quickly devolves into an increasingly violent and bizarre competition at the hands of a pagan cult.";
-const film1_backgroundImage = 'https://images.squarespace-cdn.com/content/v1/5d4ba6863e231f0001e703e7/1618289532850-IK46X694AD3REA3Y5NEF/midsommar.jpeg';
+const film1_backgroundImage = 'https://cdn.geekay.one/five-tribes-cinema-productions/midsommar.jpeg';
 const film1_alt = film1_name + ' movie banner';
 
 const film2_name = 'HEREDITARY';
 const film2_description = "When the matriarch of the Graham family passes away, her daughter and grandchildren begin to unravel cryptic and increasingly terrifying secrets about their ancestry, trying to outrun the sinister fate they have inherited.";
-const film2_backgroundImage = 'https://miro.medium.com/v2/resize:fit:1400/1*Eyd8UejypiHoK6IRvQspoQ.png';
+const film2_backgroundImage = 'https://cdn.geekay.one/five-tribes-cinema-productions/hereditary.png';
 const film2_alt = film2_name + ' movie banner';
 
 const film3_name = 'PARASITE';
 const film3_description = "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.";
-const film3_backgroundImage = 'https://static0.colliderimages.com/wordpress/wp-content/uploads/2022/10/Parasite-2019.jpg';
+const film3_backgroundImage = 'https://cdn.geekay.one/five-tribes-cinema-productions/parasite.jpg';
 const film3_alt = film3_name + ' movie banner';
 
 const Films = () => {
