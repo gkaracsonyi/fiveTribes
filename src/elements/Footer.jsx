71b3a79e37150886037c93d7a20e1a11d27ececd
gkaracsonyi <gkaracso@gmail.com>
2024-05-17 11:00:33 -0400

    remove bs and add egg

--- src/elements/Footer.jsx
+++ src/elements/Footer.jsx
@@ -19,44 +19,38 @@ const Footer = () => {
         navigate(`/${dest}`);
     };
 
+    const easterEgg = () => {
+        console.log("This site was made by Justin Getty and George Karasconyi");
+    };
+
     return (
         <>
             <footer>
-                <div className='footerLogoContainer'>
-                    <img src={ftLogo} alt="ftcp logo" />
-                </div>
-                <div className='footerLinks'>
-                    <div className='footerPageLinks'>
-                        <h3>Page Links</h3>
-                        <ul>
-                            <li><button onClick={() => goTo('about')}>About Us</button></li>
-                            <li><button onClick={() => goTo('films')}>Films</button></li>
-                            <li><button onClick={() => goTo('events')}>Events</button></li>
-                            <li><button onClick={() => goTo('commercials')}>Commercials</button></li>
-                            <li><button onClick={() => goTo('livestreaming')}>Livestreaming</button></li>
-                            <li><button onClick={() => goTo('sjl')}>SJL Racing</button></li>
-                        </ul>
+                <div className='footerMainContainer'>
+                    <div className='footerLogoContainer'>
+                        <img src={ftLogo} alt="ftcp logo" />
                     </div>
-                    <div className='footerSocialLinks'>
-                        <button><img src={ftLogo} alt="test logo" /></button>
-                        <button><img src={ftLogo} alt="test logo" /></button>
-                        <button><img src={ftLogo} alt="test logo" /></button>
-                        <button><img src={ftLogo} alt="test logo" /></button>
-                    </div>
-                    <div className="littleEasterEgg">
-
-                        <button onClick={console.log("This site was made by Justin Getty and George Karasconyi")}></button>
-                    </div>
-
-                    <div className='contactUs'>
-
-                        <p>Conttact us via our contact page *PUT CONTACT BUTTON HEREEEEEEE or reach out to us!
-                            Shoot Us An Email: info@fivetribescinemaproductions.com
-                            Or Give Us A Call: (267) 888-7206 GEORGE PLS FIX!</p>
-                        <h1>MAKE THESE BUTTONS THE SOCIAL LINKS YOU BAHRAINIAN SLUTTTTT</h1>
-
+                    <div className='footerLinks'>
+                        <div className='footerPageLinks'>
+                            <h3>Page Links</h3>
+                            <ul>
+                                <li><button onClick={() => goTo('about')}>About Us</button></li>
+                                <li><button onClick={() => goTo('films')}>Films</button></li>
+                                <li><button onClick={() => goTo('events')}>Events</button></li>
+                                <li><button onClick={() => goTo('commercials')}>Commercials</button></li>
+                                <li><button onClick={() => goTo('livestreaming')}>Livestreaming</button></li>
+                                <li><button onClick={() => goTo('sjl')}>SJL Racing</button></li>
+                            </ul>
+                        </div>
+                        <div className='footerSocialLinks'>
+                            <button><img src={ftLogo} alt="test logo" /></button>
+                            <button><img src={ftLogo} alt="test logo" /></button>
+                            <button><img src={ftLogo} alt="test logo" /></button>
+                            <button><img src={ftLogo} alt="test logo" /></button>
+                        </div>
                     </div>
                 </div>
+                <button className="littleEasterEgg" onClick={() => easterEgg()}></button>
             </footer>
         </>
     );
